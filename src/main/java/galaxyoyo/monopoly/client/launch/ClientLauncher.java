b66492e625ee0d9b781b5a185c1a89f4987b38cb
galaxyoyo <yohann.danello@gmail.com>
2014-12-17 19:23:41 +0100

    Ajout de la class principale client

---
+++ src/main/java/galaxyoyo/monopoly/client/launch/ClientLauncher.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package galaxyoyo.monopoly.client.launch;
+
+/**
+ * @author galaxyoyo
+ */
+public class ClientLauncher
+{
+	/**
+	 * @param args Arguments de démarrage, ignorés
+	 */
+	public static void main(String ... args)
+	{
+	}
+}
